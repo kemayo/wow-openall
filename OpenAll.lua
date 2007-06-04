5af6d1c75e2e00d94c7836d1afa031318f09031a
David Lynch <kemayo@gmail.com>
2007-06-04 05:43:12 +0000

    OpenAll: -Make work more reliably (darn asynchronous mail model). -Sadly, now longer than ExpressMail.

--- OpenAll.lua
+++ OpenAll.lua
@@ -1,14 +1,37 @@
-local button
-local function doNothing() end
-local function openAll()
+local deletedelay = 0.5
+local button, waitForMail, doNothing, openAll, openMail, lastopened
+function doNothing() end
+function openAll()
 	button:SetScript("OnClick", donothing)
-	for i = 1, GetInboxNumItems(), 1 do
-		if not InboxFrame:IsVisible() then break end
-		local _, _, _, _, money, COD, _, hasItem, _, wasReturned, _, canReply = GetInboxHeaderInfo(i)
-		if money > 0 then TakeInboxMoney(index)
-		elseif hasItem and COD <= 0 then TakeInboxItem(index) end
+	if GetInboxNumItems() == 0 then return end
+	openMail(1)
+end
+function openMail(index)
+	if not InboxFrame:IsVisible() then return end
+	local _, _, _, _, money, COD, _, hasItem = GetInboxHeaderInfo(index)
+	if money > 0 then TakeInboxMoney(index) end
+	elseif hasItem and COD <= 0 then TakeInboxItem(index) end
+	if GetInboxNumItems() > 1 then
+		lastopened = index
+		button:SetScript("OnUpdate", waitForMail)
+	else
+		button:SetScript("OnClick", openAll)
 	end
-	button:SetScript("OnClick", openAll)
+end
+do
+	local t = 0
+	function waitForMail()
+		t = t + arg1
+		if t > deletedelay then
+			button:SetScript("OnUpdate", nil)
+			local _, _, _, _, money, COD, _, hasItem, _, wasReturned, _, canReply = GetInboxHeaderInfo(lastopened)
+			if money > 0 or hasItem then --deleted or bumped
+				openMail(lastopened)
+			else
+				openMail(lastopened + 1)
+			end
+		end
+	end
 end
 
 button = CreateFrame("Button", "OpenAllButton", InboxFrame, "UIPanelButtonTemplate")
@@ -16,5 +39,4 @@ button:SetWidth(120)
 button:SetHeight(25)
 button:SetPoint("CENTER", InboxFrame, "TOP", -15, -410)
 button:SetText("Open All")
-button.owner = self
 button:SetScript("OnClick", openAll)
