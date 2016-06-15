1e3172b209db4773019619cbc0865497f56d38a9
David Lynch <kemayo@gmail.com>
2016-06-15 00:59:24 -0500

    Better info in the 'all' tooltip

--- OpenAll.lua
+++ OpenAll.lua
@@ -1,6 +1,6 @@
 local deletedelay, t = 0.5, 0
 local takingOnlyCash = false
-local button, button2, waitForMail, doNothing, openAll, openAllCash, openMail, lastopened, stopOpening, mail_checker, needsToWait, copper_to_pretty_money, total_cash
+local button, button2, waitForMail, doNothing, openAll, openAllCash, openMail, lastopened, stopOpening, mail_checker, needsToWait, copper_to_pretty_money, total_cash, get_total_cash
 local baseInboxFrame_OnClick
 function doNothing() end
 
@@ -99,12 +99,16 @@ end)
 button2 = makeButton("OpenAllButton2", "Take Cash", 60, 25, 20, -410)
 button2:SetScript("OnClick", openAllCash)
 
-button:SetScript("OnEnter", function()
-	GameTooltip:SetOwner(button, "ANCHOR_RIGHT")
-	GameTooltip:AddLine(string.format("%d messages", GetInboxNumItems()), 1, 1, 1)
-	GameTooltip:Show()
-end)
-button:SetScript("OnLeave", function() GameTooltip:Hide() end)
+function get_total_cash()
+	local index
+	if not total_cash then
+		total_cash = 0
+		for index=0, GetInboxNumItems() do
+			total_cash = total_cash + select(5, GetInboxHeaderInfo(index))
+		end
+	end
+	return total_cash
+end
 
 function copper_to_pretty_money(c)
 	if c > 10000 then
@@ -115,15 +119,18 @@ function copper_to_pretty_money(c)
 		return ("%d|cffeda55fc|r"):format(c%100)
 	end
 end
-button2:SetScript("OnEnter", function()
-	if not total_cash then
-		total_cash = 0
-		for index=0, GetInboxNumItems() do
-			total_cash = total_cash + select(5, GetInboxHeaderInfo(index))
-		end
-	end
+
+button:SetScript("OnEnter", function()
 	GameTooltip:SetOwner(button, "ANCHOR_RIGHT")
-	GameTooltip:AddLine(copper_to_pretty_money(total_cash), 1, 1, 1)
+	GameTooltip:AddLine(string.format("%d messages", GetInboxNumItems()), 1, 1, 1)
+	GameTooltip:AddLine(copper_to_pretty_money(get_total_cash()), 1, 1, 1)
+	GameTooltip:Show()
+end)
+button:SetScript("OnLeave", function() GameTooltip:Hide() end)
+
+button2:SetScript("OnEnter", function()
+	GameTooltip:SetOwner(button2, "ANCHOR_RIGHT")
+	GameTooltip:AddLine(copper_to_pretty_money(get_total_cash()), 1, 1, 1)
 	GameTooltip:Show()
 end)
 button2:SetScript("OnLeave", function() GameTooltip:Hide() end)
